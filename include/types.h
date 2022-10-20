4dc996e384bcf98e56bb81d2adbb0c566e9563df
Kushal-Chandar <kushalchandariit@gmail.com>
2022-10-20 22:46:17 +0530

    update: implemented strong type

--- include/types.h
+++ include/types.h
@@ -1,1 +1,14 @@
+#if !defined(TYPES)
+#define TYPES
+
+#include <string>
+
 #include "utilities/strong_types.h"
+namespace minpass {
+using Email = utilities::StrongType<std::string, struct EmailParam>;
+using Website = utilities::StrongType<std::string, struct WebsiteParam>;
+using Username = utilities::StrongType<std::string, struct UsernameParam>;
+using Password = utilities::StrongType<std::string, struct PasswordParam>;
+}  // namespace minpass
+
+#endif  // TYPES
