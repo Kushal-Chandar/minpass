6ae86e6143c787f277ef4d250296a739f5684329
Kushal-Chandar <kushalchandariit@gmail.com>
2022-12-14 23:10:43 +0530

    update(cli): added tests

---
+++ tests/cli_test.cc
@@ -0,0 +1,27 @@
+#include "cli.h"
+
+#include <drogon/drogon_test.h>  // for run
+
+#include <vector>
+
+DROGON_TEST(MinpassCliTest) {
+  // When no command line arguments are given
+  std::vector<const char *> argv{"Path to executable"};
+  CHECK(minpass::CLI::Parse(1, argv.data()) == std::nullopt);
+
+  // When --help command line arguments is given
+  argv.emplace_back("--help");
+  CHECK(minpass::CLI::Parse(2, argv.data()) != std::nullopt);
+  argv.clear();
+
+  // When -e command line arguments is given
+  argv.emplace_back("-e");
+  CHECK(minpass::CLI::Parse(2, argv.data()) != std::nullopt);
+  argv.clear();
+
+  // When both command line arguments are given (if --help is given help should
+  // print first)
+  argv.emplace_back("-v");
+  argv.emplace_back("--help");
+  CHECK(minpass::CLI::Parse(3, argv.data()) != std::nullopt);
+}
