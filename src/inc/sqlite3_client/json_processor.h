aa972892ad22dfb35dc7e6e900c378fef123a534
Kushal-Chandar <kushalchandariit@gmail.com>
2023-02-19 05:32:29 +0530

    update(wip): making a structure

---
+++ src/inc/sqlite3_client/json_processor.h
@@ -0,0 +1,32 @@
+#if !defined(MINPASS_SRC_INC_SQLITE3_CLIENT_JSON_PROCESSOR_H_)
+#define MINPASS_SRC_INC_SQLITE3_CLIENT_JSON_PROCESSOR_H_
+
+#include <drogon/drogon_callbacks.h>  // for HttpResponsePtr, HttpRequestPtr
+
+#include <optional>
+#include <tuple>  // for tuple
+
+#include "types.h"  // for Email, Password, Username
+namespace Json {
+class Value;
+}  // namespace Json
+
+namespace minpass::sqlite3_client {
+
+class JsonProcessor {
+ public:
+  static auto ParseRequestJson(const drogon::HttpRequestPtr &http_request,
+                               drogon::HttpResponsePtr &http_response,
+                               Json::Value &response_object_out)
+      -> std::optional<std::tuple<Email, Username, Password, MasterPassword>>;
+
+  static auto EncryptData(Email &email, Username &username, Password &password,
+                          MasterPassword &master_password) -> void;
+
+  static auto DecryptData(Email &email, Username &username, Password &password,
+                          MasterPassword &master_password) -> void;
+};
+
+}  // namespace minpass::sqlite3_client
+
+#endif  // MINPASS_SRC_INC_SQLITE3_CLIENT_JSON_PROCESSOR_H_
---
+++ tests/sqlite3_client/json_processor_test.cc
@@ -0,0 +1,150 @@
+#include "sqlite3_client/json_processor.h"
+
+#include <drogon/HttpRequest.h>    // for HttpRequestPtr
+#include <drogon/HttpResponse.h>   // for HttpResponse
+#include <drogon/drogon_test.h>    // for Case (ptr only), DROGON_TEST,
+#include <drogon/orm/Exception.h>  // for DrogonDbException #include
+#include <json/value.h>            // for Value
+
+#include <iostream>
+
+#include "sqlite3_client/helpers.h"
+#include "test_utilities/random_string_generator.h"
+
+const int kUsernameLen = 20;
+const int kPasswordLen = 30;
+const int kMasterPasswordLen = 30;
+
+DROGON_TEST(JsonProcessorTests_ParseRequestJson_ExceptionTest) {
+  // Testing
+  // 1. ParseRequest function should not throw even if json is null
+  Json::Value request;
+  auto http_request = drogon::HttpRequest::newHttpJsonRequest(request);
+  auto http_response = drogon::HttpResponse::newHttpJsonResponse(request);
+  Json::Value response_object;
+
+  MANDATE_NOTHROW(minpass::sqlite3_client::JsonProcessor::ParseRequestJson(
+      http_request, http_response, response_object));
+}
+
+DROGON_TEST(JsonProcessorTests_ParseRequestJson_ReturnValueTest1) {
+  // Testing
+  // 1. ParseRequest must return the correct structure bindings when json
+  // was parsed
+
+  Json::Value request;
+  request["email"] = "mail@mail.com";
+  request["password"] = minpass::tests::generate_random_string(kUsernameLen);
+  request["username"] = minpass::tests::generate_random_string(kPasswordLen);
+  request["master_password"] =
+      minpass::tests::generate_random_string(kMasterPasswordLen);
+
+  // build the json into a string
+  Json::StreamWriterBuilder builder;
+  builder["indentation"] = "";  // If you want whitespace-less output
+  const std::string output = Json::writeString(builder, request);
+
+  auto http_request = drogon::HttpRequest::newHttpRequest();
+  http_request->setBody(output);  // feed in that string
+  http_request->setContentTypeCode(
+      drogon::CT_APPLICATION_JSON);  // feed in that string
+
+  auto http_response = drogon::HttpResponse::newHttpResponse();
+  Json::Value response_object;
+
+  auto request_data = minpass::sqlite3_client::JsonProcessor::ParseRequestJson(
+      http_request, http_response, response_object);
+
+  REQUIRE(request_data.has_value() == true);
+  auto [email, username, password, master_password] = request_data.value();
+  CHECK(email.get() == request["email"]);
+  CHECK(username.get() == request["username"]);
+  CHECK(password.get() == request["password"]);
+  CHECK(master_password.get() == request["master_password"]);
+  CHECK(http_response->statusCode() == drogon::k202Accepted);
+}
+
+DROGON_TEST(JsonProcessorTests_ParseRequestJson_ReturnValueTest2) {
+  // Testing
+  // 1. ParseRequest must return empty structure bindings when json
+  // was not parsed
+
+  std::string illegal_request;
+  auto http_request = drogon::HttpRequest::newHttpRequest();
+  http_request->setBody(illegal_request);
+  http_request->setContentTypeCode(drogon::CT_APPLICATION_JSON);
+  auto http_response = drogon::HttpResponse::newHttpResponse();
+  Json::Value response_object;
+
+  auto request_data = minpass::sqlite3_client::JsonProcessor::ParseRequestJson(
+      http_request, http_response, response_object);
+
+  REQUIRE(request_data.has_value() == false);
+  CHECK(http_response->statusCode() == drogon::k400BadRequest);
+}
+
+DROGON_TEST(JsonProcessorTests_ParseRequestJson_MasterPasswordNotGiven) {
+  // Testing
+  // 1. ParseRequest must return error response when master_password is not in
+  // the request
+
+  Json::Value request;
+  request["email"] = "mail@mail.com";
+  request["password"] = minpass::tests::generate_random_string(kUsernameLen);
+  request["username"] = minpass::tests::generate_random_string(kPasswordLen);
+
+  // build the json into a string
+  Json::StreamWriterBuilder builder;
+  builder["indentation"] = "";  // If you want whitespace-less output
+  const std::string output = Json::writeString(builder, request);
+
+  auto http_request = drogon::HttpRequest::newHttpRequest();
+  http_request->setBody(output);  // feed in that string
+  http_request->setContentTypeCode(
+      drogon::CT_APPLICATION_JSON);  // feed in that string
+
+  auto http_response = drogon::HttpResponse::newHttpResponse();
+  Json::Value response_object;
+
+  auto request_data = minpass::sqlite3_client::JsonProcessor::ParseRequestJson(
+      http_request, http_response, response_object);
+
+  REQUIRE(request_data.has_value() == false);
+  CHECK(http_response->statusCode() == drogon::k400BadRequest);
+}
+
+DROGON_TEST(JsonProcessorTests_ParseRequestJson_SQLi) {
+  // Testing
+  // 1. ParseRequest must not break again SQL injections
+
+  Json::Value sql_injection;
+  sql_injection["email"] = "mail@mail.com";
+  sql_injection["username"] =
+      minpass::tests::generate_random_string(kUsernameLen).data();
+  sql_injection["password"] = "pas22sls'); AND '1' = '1'";
+  sql_injection["master_password"] =
+      minpass::tests::generate_random_string(kMasterPasswordLen).data();
+
+  // build the json into a string
+  Json::StreamWriterBuilder builder;
+  builder["indentation"] = "";  // If you want whitespace-less output
+  const std::string output = Json::writeString(builder, sql_injection);
+
+  auto http_request = drogon::HttpRequest::newHttpRequest();
+  http_request->setBody(output);  // feed in that string
+  http_request->setContentTypeCode(
+      drogon::CT_APPLICATION_JSON);  // feed in that string
+  auto http_response = drogon::HttpResponse::newHttpResponse();
+  Json::Value response_object;
+
+  auto request_data = minpass::sqlite3_client::JsonProcessor::ParseRequestJson(
+      http_request, http_response, response_object);
+
+  REQUIRE(request_data.has_value() == true);
+  auto [email, username, password, master_password] = request_data.value();
+  CHECK(email.get() == sql_injection["email"]);
+  CHECK(username.get() == sql_injection["username"]);
+  CHECK(password.get() == sql_injection["password"]);
+  CHECK(master_password.get() == sql_injection["master_password"]);
+  CHECK(http_response->statusCode() == drogon::k202Accepted);
+}
